bf939dded6efc1d3b44c4b82089530ac84e3fe95
Paula Teel <teelpaulaj@johndeere.com>
2021-03-12 12:55:46 -0600

    First version of back-end API completed

--- controllers/paintingsController.js
+++ controllers/paintingsController.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const PaintingModel = require('../models').Paintings;
 
 
+// GET ALL Paintings
+router.get("/", async (req, res) => {
+    let allPaintings = await PaintingModel.findAll();
+    res.json({ allPaintings});
+  });
 
 
 
--- server.js
+++ server.js
@@ -7,22 +7,6 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const createError = require("http-errors");
 
-const verifyToken = (req, res, next) => {
-  let token = req.cookies.jwt;
-  // COOKIE PARSER GIVES YOU A .cookies PROP, WE NAMED OUR TOKEN jwt
-  console.log("Cookies: ", req.cookies.jwt);
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
-    if (err || !decodedUser) {
-      return res.status(401).json({ error: "Unauthorized Request" });
-    }
-    req.user = decodedUser;
-    // ADDS A .user PROP TO REQ FOR TOKEN USER
-    console.log(decodedUser);
-
-    next();
-  });
-};
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
